83edd0e942cf7e8638966f39ab868cc0dabaad6a
Austin Derbique <austin@derbique.org>
2021-03-06 14:11:13 -0700

    putting it all together...

--- classifier/main.py
+++ classifier/main.py
@@ -45,7 +45,32 @@ def process_image(s3_object_path):
     # Download the object to /tmp
     s3_resource.Object(s.bucket, s.key).download_file(f'/tmp/{s.key}')
 
-    
+    #Run classificaiton on image
+    result = classify(f'/tmp/{s.key}')
 
-#while get_num_messages_available() > 0:
-    #print("Time to process an image")
+    # Delete file
+    os.remove(f'/tmp/{s.key}')
+
+    #Write result as tag to original file
+
+    put_tags_response = s3.put_object_tagging(
+        Bucket=s.bucket,
+        Key=s.key,    
+        Tagging={
+            'TagSet': [
+                {
+                    'Key': 'Classification',
+                    'Value': result
+                },
+            ]
+        }
+    )
+
+while get_num_messages_available() > 0:
+    print("Retrieving Image Link from SQS")
+    s3_object_path, receipt_handle = get_latest_message()
+    process_image(s3_object_path)
+    delete_message(receipt_handle)
+    print("Successfully Processed {}".format(s3_object_path))
+
+print("Job Complete")
