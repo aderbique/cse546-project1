15f56b6c3947079bf039af54d6bb55146ddd9aa5
apappas16 <apappas5@asu.edu>
2021-03-11 15:51:34 -0700

    added results page and button

--- server/app.py
+++ server/app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, request, redirect, url_for, abort
 from werkzeug.utils import secure_filename
 import boto3
 from botocore.exceptions import NoCredentialsError
+from boto3.dynamodb.conditions import Key
+import get_classifications
+from natsort import natsorted
+
+
 
 app = Flask(__name__)
 #app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
@@ -38,6 +43,13 @@ def upload_file_to_s3(file, bucket_name):
 
     return "{}{}".format(app.config["S3_LOCATION"], file.filename)
 
+
+def get_output_from_dynamo():
+    print("Getting items in DynamoDB Table...")
+    results = get_classifications.get_classifications_dict()
+    results_sorted = get_classifications.natsort_dict(results)
+    return results_sorted
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -53,6 +65,12 @@ def upload_files():
                 # Get the queue. This returns an SQS.Queue instance
                 sqs.send_message(QueueUrl=queue_url,MessageBody=uploaded, MessageGroupId='messageGroup1')
     return redirect(url_for('index'))
+    
+@app.route('/results', methods=['POST'])
+def get_results():
+    results = get_output_from_dynamo()
+    print("Finished")
+    return render_template('results.html', results=results)
 
 app.run(
     host=os.getenv('LISTEN', '0.0.0.0'),
